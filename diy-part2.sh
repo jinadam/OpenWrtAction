7aac2322998528b038b898d023d32c41ca76a258
jinadam <temp@uooo.eu.org>
2024-09-05 21:28:46 +0800

    fix: update

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,6 +14,8 @@
 sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/downloads/luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
